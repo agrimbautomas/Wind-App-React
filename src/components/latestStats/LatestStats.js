37934c23d694b0f941d81a6255d61e304d007205
Tomas Agrimbau <tomas@amalgama.co>
2019-06-28 19:28:40 -0300

    Add wind direction based on degrees

---
+++ src/components/latestStats/LatestStats.js
@@ -0,0 +1,81 @@
+import React, {Component} from "react";
+import './LatestStats.scss';
+
+class LatestStats extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            direction: this.parseDirection(this.props.direction),
+            speed: this.props.speed,
+            gust: this.props.gust,
+            messure: 'kts',
+            in_knots: true
+
+        };
+        //this.fadeImage = this.fadeImage.click(this);
+    }
+
+    fadeImage = () => {
+        if (this.state.in_knots)
+            this.switchToKms();
+        else
+            this.switchToKnots();
+    }
+
+    switchToKms = () => {
+        this.setState({
+            speed: this.knToKm(this.state.speed),
+            gust: this.knToKm(this.state.gust),
+            messure: 'km/h',
+            in_knots: false
+        })
+    }
+
+    switchToKnots = () => {
+        this.setState({
+            speed: this.kmToKn(this.state.speed),
+            gust: this.kmToKn(this.state.gust),
+            messure: 'kts',
+            in_knots: true
+        })
+    }
+
+    kmToKn = (kms) => {
+        return Number((kms / 1.852).toFixed(1))
+    }
+
+
+    knToKm = (knots) => {
+        return Number((knots * 1.852).toFixed(1))
+    }
+
+    parseDirection = (direction) => {
+        return Math.round(direction);
+    }
+
+    getDirection = (angle) => {
+        // var directions = ["North", "North-East", "East", "South-East", "South", "South-West", "West", "North-West"]
+        var directions = ['Norte', 'Noreste', 'Este', 'Sudeste', 'Sur', 'Sudoeste', 'Oeste', 'Noroeste'];
+
+        return directions[Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8];
+    }
+
+
+    render() {
+        return (
+            <div
+                className="latest-stats-item" onClick={this.fadeImage}>
+                <h2>{this.state.direction} °</h2>
+                <h2>{this.getDirection(this.state.direction)}</h2>
+                <h1>{this.state.speed} {this.state.messure}</h1>
+                <h3>{this.state.gust} {this.state.messure}</h3>
+                <span className='id-hidden'>{this.props.id}</span>
+
+            </div>
+        );
+    }
+}
+
+export default LatestStats;
--- src/components/windLogsList/WindLogsList.js
+++ src/components/windLogsList/WindLogsList.js
@@ -1,10 +1,9 @@
 import React from "react";
-import WindLog from '../windLog/WindLog';
+import LatestStats from '../latestStats/LatestStats';
 import SocketsService from "../../services/socketsService"
 import './WindLogsList.scss';
 
 const ENDPOINT = "http://wind-api.amalgama.co/1";
-
 //const ENDPOINT = "http://localhost:3000/1";
 
 
@@ -13,7 +12,7 @@ class WindLogsList extends React.Component {
     constructor(props) {
         super(props);
 
-        this.get_windLogs_url = ENDPOINT + "/stats";
+        this.get_stats_url = ENDPOINT + "/stats";
         SocketsService.listenForNewWindLogs(this);
 
         this.state = {
@@ -24,7 +23,7 @@ class WindLogsList extends React.Component {
     }
 
     componentDidMount = () => {
-        fetch(this.get_windLogs_url)
+        fetch(this.get_stats_url)
             .then(res => res.json())
             .then(
                 (results) => {
@@ -43,14 +42,14 @@ class WindLogsList extends React.Component {
     }
 
 
-    getWindLog = (windLog) => {
+    getLatestStatsView = (stats) => {
         return (
-            <WindLog
-                key={windLog.id}
-                id={windLog.id}
-                speed={windLog.speed}
-                gust={windLog.gust}
-                direction={windLog.direction}
+            <LatestStats
+                key={stats.id}
+                id={stats.id}
+                speed={stats.speed}
+                gust={stats.gust}
+                direction={stats.direction}
                 className={'A-donde-va'}
             />
         );
@@ -75,24 +74,22 @@ class WindLogsList extends React.Component {
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
-            return this.renderWindLogsView()
+            return this.renderLatestStatsView()
         }
     }
 
 
-    renderWindLogsView = () => {
+    renderLatestStatsView = () => {
         if (this.state.stats)
             return this.displayStats();
         else
             return this.displayPlaceholder();
     }
 
-
     displayStats = () => {
-        var windLog = this.state.stats.latest;
         return (
             <div className="stats-container">
-                {this.getWindLog(windLog)}
+                {this.getLatestStatsView(this.state.stats.latest)}
             </div>
         )
     }
@@ -100,7 +97,7 @@ class WindLogsList extends React.Component {
     displayPlaceholder = () => {
         return (
             <div className="stats-placeholder">
-                <h1>No hay ordenes pendientes</h1>
+                <h1>No información disponible</h1>
             </div>
         )
     }
