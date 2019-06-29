046f248fa2453e137398b2c431f44691bd61231c
Tomas Agrimbau <tomas@amalgama.co>
2019-06-29 10:43:35 -0300

    Add new domain

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
 
     render() {
         return (
-            <img alt="Logo HoySeNavega" />
+            <img alt="Logo VientoEnElRio" />
         )
     }
 }
--- src/components/mainStatsContainer/MainStatsContainer.js
+++ src/components/mainStatsContainer/MainStatsContainer.js
@@ -4,7 +4,7 @@ import SocketsService from "../../services/socketsService"
 import './MainStatsContainer.scss';
 import spinner from '../..//assets/img/spinner.gif';
 
-const ENDPOINT = "http://wind-api.amalgama.co/1";
+const ENDPOINT = "http://api.vientoenelrio.com/1";
 
 //const ENDPOINT = "http://localhost:3000/1";
 
