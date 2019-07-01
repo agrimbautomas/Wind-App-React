480081305f3848ae85d481b85c704595f607c014
Tomas Agrimbau <tomas@amalgama.co>
2019-07-01 17:40:26 -0300

    Add loading empty to maps

--- src/components/mapView/MapView.js
+++ src/components/mapView/MapView.js
@@ -5,23 +5,21 @@ import {Map, GoogleApiWrapper} from 'google-maps-react';
 
 // Import custom styles to customize the style of Google Map
 const styles = require('./GoogleMapStyles.json')
-
+const EmptyLoadingContainer = () => (
+    <span></span>
+)
 
 class MapView extends Component {
 
     render() {
-        const style = {
-            width: '50vw',
-            height: '75vh',
-            'marginLeft': 'auto',
-            'marginRight': 'auto'
-        }
+
         return (
             <Map className="map-container"
                  google={this.props.google}
                  zoom={13}
                  styles={styles}
                  disableDefaultUI={true}
+
                  initialCenter={{
                      lat: -34.46000,
                      lng: -58.50458
@@ -33,7 +31,8 @@ class MapView extends Component {
 
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyAHvCZF6bk27fO-ieRetP066BSoYLkqD94'
+    apiKey: 'AIzaSyAHvCZF6bk27fO-ieRetP066BSoYLkqD94',
+    LoadingContainer: EmptyLoadingContainer
 })(MapView);
 
 
